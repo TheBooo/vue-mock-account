ad669e2ee1436792207e6ec4e4b8be913c5cad9b
Ivan Egorov <fearntn@yandex.ru>
2020-07-14 17:07:28 +0300

    logout icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,16 @@
   <header class="header">
     <img alt="Vue logo" src="../assets/logo.png" class="logo" />
     <h1>Контакты</h1>
-    <button @click="exit" class="btn btn-exit">Выйти</button>
+    <button @click="exit" class="btn btn-exit">Выйти<Logout /></button>
   </header>
 </template>
 
 <script>
+import Logout from "vue-material-design-icons/Logout";
+
 export default {
   name: "Header",
+  components: { Logout },
   methods: {
     exit() {
       sessionStorage.clear();
@@ -33,5 +36,7 @@ export default {
   position: absolute;
   right: 5px;
   top: 5px;
+  display: flex;
+  align-items: center;
 }
 </style>
